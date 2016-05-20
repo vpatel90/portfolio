d1c68ee3f7367430fdce0e594373b334e406d42f
Vivek Patel <vnp229@gmail.com>
2016-05-19 20:37:30 -0500

    working on home page

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,1 +1,30 @@
 // This is where it all goes :)
+//= require jquery
+var ready;
+ready = function() {
+	var home = document.getElementById("home");
+	var projects = document.getElementById("projects");
+	var contact = document.getElementById("contact");
+	var underline = document.getElementById("underline");
+
+	home.addEventListener('click', function(){
+		underline.style.left = "0.5em";
+		underline.style.width = "4em";
+
+	});
+
+	projects.addEventListener('click', function(){
+		underline.style.left = "6em";
+		underline.style.width = "5em";
+
+	});
+
+	contact.addEventListener('click', function(){
+		underline.style.left = "12.5em";
+		underline.style.width = "5em";
+
+	});
+}
+
+$(document).ready(ready);
+$(document).on('page:load', ready);
